e7d71dd81065cb16da6f59bb77ddaecbc28c38c1
Marko <markommilanovic92@gmail.com>
2020-05-12 13:06:18 +0200

    cart util to group items in cart reducer

--- src/utils/cartUtils.js
+++ src/utils/cartUtils.js
@@ -1,8 +1,10 @@
+//checking if item we are trying to add to cart already exist
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToAdd.id
   );
 
+  //if item already exist in cart, we are increasing quantity prop by 1
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
@@ -11,5 +13,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
   }
 
+  //if item doesn't exist in cart we return item with new prop of quantity: 1
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
