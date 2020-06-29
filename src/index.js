118e3c222f668ce1293ed83a2ce18a9e0b5ea316
Marko <markommilanovic92@gmail.com>
2020-06-29 14:19:23 +0200

    removing redux-logger from production build

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 
-const store = createStore(reducers, applyMiddleware(logger));
+const middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+const store = createStore(reducers, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
 
 ReactDOM.render(
